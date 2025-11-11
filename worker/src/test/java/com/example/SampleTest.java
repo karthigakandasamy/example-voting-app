fde129dc800ea15473bf67b8c5cf202c1d3790c8
karthigakandasamy <karthiga.kandasamy@globallogic.com>
2025-11-11 11:08:38 +0530

    Create sample Tests

---
+++ worker/src/test/java/com/example/SampleTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import io.qameta.allure.Description;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SampleTest {
+
+    @Test
+    @Description("Verify basic functionality")
+    void verifyTrueCondition() {
+        assertTrue(true);
+    }
+}
