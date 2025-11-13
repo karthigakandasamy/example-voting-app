656680cc27a169ddf613fc5698445f9827c25b6b
karthigakandasamy <karthiga.kandasamy@globallogic.com>
2025-11-13 11:59:36 +0530

    Update generate_allure_env.py

--- worker/generate_allure_env.py
+++ worker/generate_allure_env.py
@@ -1,10 +1,10 @@
 import json
 
-with open('allure-results/sonarqube-metrics.json', 'r') as f:
+with open('/var/lib/jenkins/workspace/Allure_TestReport/worker/allure-results/sonarqube-metrics.json', 'r') as f:
     data = json.load(f)
 
 measures = data['component']['measures']
-with open('allure-results/environment.properties', 'w') as env_file:
+with open('/var/lib/jenkins/workspace/Allure_TestReport/worker/allure-results/environment.properties', 'w') as env_file:
     for measure in measures:
         # Format key=value for the properties file
         env_file.write(f"{measure['metric']}={measure['value']}\n")
