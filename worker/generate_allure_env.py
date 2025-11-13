1bc167b9cb98f3bafdc8f3f2d41dc7835d3c6081
karthigakandasamy <karthiga.kandasamy@globallogic.com>
2025-11-13 15:20:57 +0530

    Update generate_allure_env.py

--- worker/generate_allure_env.py
+++ worker/generate_allure_env.py
@@ -16,8 +16,9 @@ else:
     sonar_dashboard_url = f"https://172.30.117.227:9000/dashboard?id={project_key}"
 
 with open('/var/lib/jenkins/workspace/Allure_TestReport/worker/allure-results/environment.properties', 'w') as env_file:
+    env_file.write(f"SonarQube_Dashboard={sonar_dashboard_url}\n")
     for measure in measures:
         # Format key=value for the properties file
         env_file.write(f"{measure['metric']}={measure['value']}\n")
-        env_file.write(f"SonarQube_Dashboard={sonar_dashboard_url}\n")
+    
         
