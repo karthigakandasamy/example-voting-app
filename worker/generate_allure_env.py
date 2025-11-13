889d793f889c1237859445d9a83ff515c6513813
karthigakandasamy <karthiga.kandasamy@globallogic.com>
2025-11-13 14:51:32 +0530

    Update generate_allure_env.py

--- worker/generate_allure_env.py
+++ worker/generate_allure_env.py
@@ -4,7 +4,20 @@ with open('/var/lib/jenkins/workspace/Allure_TestReport/worker/allure-results/so
     data = json.load(f)
 
 measures = data['component']['measures']
+
+sonar_host_url = os.getenv("SONAR_HOST_URL", "").rstrip("/")
+project_key = os.getenv("projectKey", "unknown_project")
+sonar_env = os.getenv("SONARQUBE_ENV", "SonarQube")
+
+if sonar_host_url:
+    sonar_dashboard_url = f"{sonar_host_url}/dashboard?id={project_key}"
+else:
+    sonar_dashboard_url = f"https://172.30.117.227:9000/dashboard?id={project_key}"
+
 with open('/var/lib/jenkins/workspace/Allure_TestReport/worker/allure-results/environment.properties', 'w') as env_file:
     for measure in measures:
         # Format key=value for the properties file
         env_file.write(f"{measure['metric']}={measure['value']}\n")
+        env_file.write(f"SonarQube Instance={sonar_env}\n")
+        env_file.write(f"SonarQube Dashboard={sonar_dashboard_url}\n")
+        
