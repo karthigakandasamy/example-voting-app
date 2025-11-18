5b8dbf9ec5eb14f4c0fbb5c86a2b6e3a4f68ed25
karthigakandasamy <karthiga.kandasamy@globallogic.com>
2025-11-18 17:57:01 +0530

    Create ApiHealthCheckTest.java

---
+++ worker/src/test/java/unit/ApiHealthCheckTest.java
@@ -0,0 +1,13 @@
+package unit;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ApiHealthCheckTest {
+
+    @Test
+    public void checkApiStatus() {
+        int statusCode = 200; // mock
+        Assert.assertEquals(statusCode, 200, "API is not healthy");
+    }
+}
